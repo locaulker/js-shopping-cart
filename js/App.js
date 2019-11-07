59f28ab08aa598a21f9a306dd9ff4e1a1de5d4af
Lawrence <Caulker>
2019-11-06 22:27:51 -0500

    updated initial setup

--- js/App.js
+++ js/App.js
@@ -1,1 +1,21 @@
 
+const removeCartItemButtons = document.getElementsByClassName('btn-danger') 
+
+console.log(removeCartItemButtons);
+
+// Remove Item From Cart
+for (let i = 0; i < removeCartItemButtons.length; i++) {
+  const button = removeCartItemButtons[i];
+  button.addEventListener('click', (e) => {
+    const buttonClicked = e.target
+    buttonClicked.parentElement.parentElement.remove();
+    updateCartTotal();
+  });
+}
+
+
+// Update Cart Total
+updateCartTotal = () => {
+  const cartItemContainer = getElementsByClassName('cart-items')[0];
+  const cartRows = cartItemContainer.getElementsByClassName('cart-row')
+}
